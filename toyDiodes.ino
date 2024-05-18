780ab4e289905e8cefcb5cb17aa0759688c1e892
Filip <filipmymail@gmail.com>
2024-05-18 20:32:26 +0200

    Added array to set pins

--- toyDiodes.ino
+++ toyDiodes.ino
@@ -1,3 +1,6 @@
+#define pinLen 5
+uint8_t p[pinLen] = {4, 5, 6, 7, 8};
+
 const uint8_t patternPin = 3;
 const uint8_t speedPin = 2;
 
@@ -8,29 +11,29 @@ uint16_t speed = defaultSpeed;
 uint8_t pattern = 0;
 uint8_t patternIndex = 0;
 
-const uint8_t patterns[patternCount][10][5] = {
-  {{8}, {7}, {6}, {5}, {4}},
-  //{{4}, {5}, {6}, {7}, {8}},
-  {{4}, {4, 5}, {5, 6}, {6, 7}, {7,8}, {8}, {}},
-  {{8, 4}, {7, 5}, {6}},
-  {{6}, {7, 5}, {8, 4}},
-  //{{8}, {8, 7}, {8, 7, 6}, {8, 7, 6, 5}, {8, 7, 6, 5, 4}, {7, 6, 5, 4}, {6, 5, 4}, {5, 4}, {4}, {}},
-  {{4}, {5, 4}, {6, 5, 4}, {7, 6, 5, 4}, {8, 7, 6, 5, 4}, {8, 7, 6, 5}, {8, 7, 6}, {8, 7}, {8}, {}},
-  {{8}, {6}, {7}, {5}, {6}, {4}, {5}},
-  //{{4}, {6}, {5}, {7}, {6}, {8}, {7}},
-  //{{8, 7}, {8}, {7, 6}, {7}, {6, 5}, {6}, {5, 4}, {5}, {4, 8}, {4}},
-  {{4, 5}, {4}, {5, 6}, {5}, {6, 7}, {6}, {7, 8}, {7}, {8, 4}, {8}},
-  {{4, 5, 6, 7, 8}, {}}
+const uint8_t patterns[patternCount][10][pinLen] = {
+{{p[4]}, {p[3]}, {p[2]}, {p[1]}, {p[0]}},
+//{{p[0]}, {p[1]}, {p[2]}, {p[3]}, {p[4]}},
+{{p[0]}, {p[0], p[1]}, {p[1], p[2]}, {p[2], p[3]}, {p[3],p[4]}, {p[4]}, {}},
+{{p[4], p[0]}, {p[3], p[1]}, {p[2]}},
+{{p[2]}, {p[3], p[1]}, {p[4], p[0]}},
+//{{p[4]}, {p[4], p[3]}, {p[4], p[3], p[2]}, {p[4], p[3], p[2], p[1]}, {p[4], p[3], p[2], p[1], p[0]}, {p[3], p[2], p[1], p[0]}, {p[2], p[1], p[0]}, {p[1], p[0]}, {p[0]}, {}},
+{{p[0]}, {p[1], p[0]}, {p[2], p[1], p[0]}, {p[3], p[2], p[1], p[0]}, {p[4], p[3], p[2], p[1], p[0]}, {p[4], p[3], p[2], p[1]}, {p[4], p[3], p[2]}, {p[4], p[3]}, {p[4]}, {}},
+{{p[4]}, {p[2]}, {p[3]}, {p[1]}, {p[2]}, {p[0]}, {p[1]}},
+//{{p[0]}, {p[2]}, {p[1]}, {p[3]}, {p[2]}, {p[4]}, {p[3]}},
+//{{p[4], p[3]}, {p[4]}, {p[3], p[2]}, {p[3]}, {p[2], p[1]}, {p[2]}, {p[1], p[0]}, {p[1]}, {p[0], p[4]}, {p[0]}},
+{{p[0], p[1]}, {p[0]}, {p[1], p[2]}, {p[1]}, {p[2], p[3]}, {p[2]}, {p[3], p[4]}, {p[3]}, {p[4], p[0]}, {p[4]}},
+{{p[0], p[1], p[2], p[3], p[4]}, {}}
 };
 
 const uint8_t patternSize[patternCount] = {5, 7, 3, 3, 10, 7, 10, 2};
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(patternPin, INPUT_PULLUP);
+  pinMode(speedPin, INPUT_PULLUP);
 
-  for (uint8_t i = 4; i < 10; i++)
-    pinMode(i, OUTPUT);
+  for (uint8_t pin : p)
+    pinMode(pin, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(patternPin), changePattern, FALLING);
   attachInterrupt(digitalPinToInterrupt(speedPin), changeSpeed, FALLING);
@@ -42,7 +45,7 @@ void loop() {
 }
 
 void showPatternStep() {
-  for (uint8_t i = 4; i < 9; i++)
+  for (uint8_t i = 9; i <= 13; i++)
     digitalWrite(i, LOW);
 
   for (uint8_t i = 0; i < sizeof(patterns[pattern][patternIndex]); i++)
